3f1cd9c7ce99b0b707d8e374cae1491048e9a7b4
Jeremy-Zhu <zhuxiaorui1989@gmail.com>
2023-04-12 01:13:05 -0400

    Remove data argument and fix formula loading

--- tests/testthat/test-surr_rsq_rank.R
+++ tests/testthat/test-surr_rsq_rank.R
@@ -1,7 +1,7 @@
 test_that("test surr_rsq_rank", {
   # library(SurrogateRsq)
   library(MASS)
-
+  library(dplyr)
   # Read the csv file of the white wine data
   data("WhiteWine")
 
@@ -14,43 +14,40 @@ test_that("test surr_rsq_rank", {
   full_formula <- as.formula(paste(y, paste(x_names, collapse = " + "), sep = " ~ "))
 
   # allsig_formula <- update(full_formula, ~ . - citric.acid - chlorides - total.sulfur.dioxide)
-  allsig_formula <- as.formula(quality ~ fixed.acidity + volatile.acidity + residual.sugar +
+  allsig_formula <- as.formula("quality ~ fixed.acidity + volatile.acidity + residual.sugar +
                                free.sulfur.dioxide + density +
-                               pH + sulphates + alcohol)
+                               pH + sulphates + alcohol")
 
   # sele_formula <- update(full_formula, ~ . - total.sulfur.dioxide - chlorides - density - citric.acid)
-  sele_formula <- as.formula(quality ~ fixed.acidity + volatile.acidity +
+  sele_formula <- as.formula("quality ~ fixed.acidity + volatile.acidity +
                                residual.sugar + free.sulfur.dioxide +
-                               pH + sulphates + alcohol)
-  # full_mod <- polr(formula = full_formula,
-  #                  data = WhiteWine,
-  #                  method = "probit")
-  #
-  # allsig_mod <- polr(formula = allsig_formula,
-  #                    data = WhiteWine,
-  #                    method = "probit")
-  # sele_mod <- polr(formula = sele_formula,
-  #                    data = WhiteWine,
-  #                    method = "probit")
-  #
-  # sur2 <- surr_rsq(model = allsig_mod,
-  #                  full_model = full_mod,
-  #                  data = WhiteWine,
-  #                  avg.num = 30)
-  #
-  # rank_tab_sur2 <- surr_rsq_rank(object  = sur2,
-  #                                data    = WhiteWine,
-  #                                avg.num = 30)
-  #
-  # sur3 <- surr_rsq(model = sele_mod,
-  #                  full_model = sele_mod,
-  #                  data = WhiteWine,
-  #                  avg.num = 30)
-  #
-  # rank_tab_sur3 <- surr_rsq_rank(object  = sur3,
-  #                                data    = WhiteWine,
-  #                                avg.num = 30)
-  #
+                               pH + sulphates + alcohol")
+
+  full_mod <- polr(formula = full_formula,
+                   data = WhiteWine,
+                   method = "probit")
+
+  allsig_mod <- polr(formula = allsig_formula,
+                     data = WhiteWine,
+                     method = "probit")
+  sele_mod <- polr(formula = sele_formula,
+                     data = WhiteWine,
+                     method = "probit")
+
+  sur2 <- surr_rsq(model = allsig_mod,
+                   full_model = full_mod,
+                   avg.num = 30)
+
+  rank_tab_sur2 <- surr_rsq_rank(object  = sur2,
+                                 avg.num = 30)
+
+  sur3 <- surr_rsq(model = sele_mod,
+                   full_model = sele_mod,
+                   avg.num = 30)
+
+  rank_tab_sur3 <- surr_rsq_rank(object  = sur3,
+                                 avg.num = 30)
+
   # var_set <- list(c("alcohol", "volatile.acidity"),
   #                 c("residual.sugar", "free.sulfur.dioxide"))
   #
@@ -61,30 +58,29 @@ test_that("test surr_rsq_rank", {
   #                 avg.num = 30)
   # # print(Rank_table2, digits = 2)
   #
-  # expect_true(!is.null(rank_tab_sur2), "list")
-  # expect_true(!is.null(rank_tab_sur3), "list")
+  expect_true(!is.null(rank_tab_sur2), "list")
+  expect_true(!is.null(rank_tab_sur3), "list")
 
 })
 
 test_that("test surr_rsq_rank example", {
-  # library(MASS)
-  # data("WhiteWine")
-  #
-  # sele_formula <- as.formula(quality ~ fixed.acidity + volatile.acidity +
-  #                              residual.sugar + free.sulfur.dioxide +
-  #                              pH + sulphates + alcohol)
-  #
-  # sele_mod <- polr(formula = sele_formula,
-  #                  data = WhiteWine,
-  #                  method = "probit")
-  #
-  # sur1 <- surr_rsq(model = sele_mod,
-  #                  full_model = sele_mod,
-  #                  data = WhiteWine,
-  #                  avg.num = 100)
-  #
-  # rank_tab_sur1 <- surr_rsq_rank(object  = sur1,
-  #                                data    = WhiteWine,
-  #                                avg.num = 30)
-  # expect_true(!is.null(rank_tab_sur1), "list")
+  library(MASS)
+  data("WhiteWine")
+
+  sele_formula <- as.formula("quality ~ fixed.acidity + volatile.acidity +
+                               residual.sugar + free.sulfur.dioxide +
+                               pH + sulphates + alcohol")
+
+  sele_mod <- polr(formula = sele_formula,
+                   data = WhiteWine,
+                   method = "probit")
+
+  sur1 <- surr_rsq(model = sele_mod,
+                   full_model = sele_mod,
+                   avg.num = 100)
+
+  rank_tab_sur1 <- surr_rsq_rank(object  = sur1,
+                                 avg.num = 30)
+  expect_true(!is.null(rank_tab_sur1), "list")
+
 })
